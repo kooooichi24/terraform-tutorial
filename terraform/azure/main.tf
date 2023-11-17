fdf4ebdb53c527409984001b7079681dc46e0747
kooooichi24 <ex.kouichi1224@gmail.com>
2023-11-17 15:37:26 +0900

    infra: azure-tutorial

---
+++ terraform/azure/main.tf
@@ -0,0 +1,39 @@
+# Configure the Azure provider
+terraform {
+  required_version = ">= 1.1.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  cloud {
+    organization = "koichi-furukawa"
+    workspaces {
+      name = "learn-terraform-azure"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = "japaneast"
+  
+  tags = {
+    Environment = "Terraform Getting Started"
+    Team = "DevOps"
+  }
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "japaneast"
+  resource_group_name = azurerm_resource_group.rg.name
+}
