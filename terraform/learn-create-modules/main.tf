4a60b06aadfa71f6b29b4aa8dda8f6a6092e6a36
kooooichi24 <ex.kouichi1224@gmail.com>
2023-11-19 19:54:12 +0900

    rename:

--- terraform/learn-create-modules/main.tf
+++ terraform/learn-create-modules/main.tf
@@ -2,5 +2,5 @@ module "azuread-application" {
   source  = "./modules/azuread-application"
 
   display_name = "learn-create-modules"
-  description  = "This is a sample application."
+  description  = "This is a sample application for creating modules."
 }
