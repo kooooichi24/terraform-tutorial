c16ff655a2363c7c3e08526cd663b6228ff7cbfb
kooooichi24 <ex.kouichi1224@gmail.com>
2023-11-19 19:15:38 +0900

    fix: output value

--- terraform/learn-create-modules/modules/azuread-application/outputs.tf
+++ terraform/learn-create-modules/modules/azuread-application/outputs.tf
@@ -1,4 +1,4 @@
 output "client_id" {
   description = "The Client ID of the Application"
-  value       = azuread_application.application.application_id
+  value       = azuread_application.application.client_id
 }
